8ec1418b3265511ee3e03d3db653733736dfcbf8
Sugini Injani <suginiinjani@students.unnes.ac.id>
2024-11-20 19:30:51 +0700

    change to uppercase

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header({ title }) {
     <header className='py-3 mb-4' style={{ marginTop: '60px' }}>
         <div className='container d-flex flex-wrap justify-content-center'>
             <a href="/" className='quicksand-700 fs-1 d-flex align-items-center mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none'>
-                <span>{title}</span>
+                <span>{title.toUpperCase()}</span>
             </a>
         </div>
     </header>
